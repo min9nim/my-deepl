be287baac1270f6e9d50b0efddaf201d9055e000
keating <min9nim@gmail.com>
2023-12-05 23:57:37 +0900

    fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,30 @@ export default function Home() {
           value={text}
           rows={10}
           cols={100}
-          onChange={(e) => setText(e.target.value)}
+          onChange={e => {
+            setText(e.target.value)
+            traslate(e.target.value, setTranslated)
+          }}
         ></textarea>
         <pre>{traslated}</pre>
       </div>
     </main>
   )
+}
+
+const traslate = debounce(async (text, setTranslated) => {
+  const result = await fetch('/api/translate', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ text }),
+  }).then(res => res.json())
+  setTranslated(result.message)
+}, 500)
+
+function debounce(func, timeout = 300) {
+  let timer
+  return (...args) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => func(...args), timeout)
+  }
 }
