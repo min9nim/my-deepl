1404e289017a31e87a69cb7dbcb66abcb4df79a9
keating <min9nim@gmail.com>
2023-12-06 19:36:05 +0900

    deepl api 사용

--- src/app/api/translate/route.ts
+++ src/app/api/translate/route.ts
@@ -4,11 +4,10 @@ export async function POST(request: Request) {
   try {
     const { text } = await request.json()
 
-    // const result = await deeplApi(text)
-    // console.log(result)
+    const result = await deeplApi(text)
 
     return NextResponse.json(
-      { message: text + '\n\n\n' + text },
+      { message: text + '\n---\n' + result.translations[0].text },
       {
         status: 200,
       },
@@ -24,72 +23,11 @@ export async function POST(request: Request) {
 }
 
 const deeplApi = text =>
-  fetch(`https://www2.deepl.com/jsonrpc?method=LMT_handle_jobs`, {
+  fetch(`https://api-free.deepl.com/v2/translate`, {
     method: 'post',
     headers: {
       'Content-Type': 'application/json',
+      Authorization: `DeepL-Auth-Key ${process.env.API_KEY}`,
     },
-    body: JSON.stringify({
-      jsonrpc: '2.0',
-      method: 'LMT_handle_jobs',
-      params: {
-        jobs: [
-          {
-            kind: 'default',
-            sentences: [{ text, id: 1, prefix: '' }],
-            raw_en_context_before: [],
-            raw_en_context_after: [],
-            preferred_num_beams: 4,
-            quality: 'fast',
-          },
-        ],
-        lang: {
-          target_lang: 'EN',
-          preference: {
-            weight: {
-              DE: 0.13683,
-              EN: 4.36896,
-              ES: 0.10062,
-              FR: 0.11709,
-              IT: 0.07283,
-              JA: 0.07654,
-              NL: 0.07403,
-              PL: 0.06941,
-              PT: 0.06633,
-              RU: 0.07474,
-              ZH: 0.06666,
-              BG: 0.05156,
-              CS: 0.05317,
-              DA: 0.05052,
-              EL: 0.0488,
-              ET: 0.04862,
-              FI: 0.05556,
-              HU: 0.04994,
-              ID: 0.04914,
-              LV: 0.03945,
-              LT: 0.04608,
-              RO: 0.04869,
-              SK: 0.04841,
-              SL: 0.0472,
-              SV: 0.05872,
-              TR: 0.0505,
-              UK: 0.05918,
-              KO: 12.90118,
-              NB: 0.05554,
-            },
-            default: 'default',
-          },
-          source_lang_user_selected: 'auto',
-        },
-        priority: -1,
-        commonJobParams: {
-          regionalVariant: 'en-US',
-          mode: 'translate',
-          textType: 'plaintext',
-          browserType: 1,
-        },
-        timestamp: 1701784480969,
-      },
-      id: 11050016,
-    }),
+    body: JSON.stringify({ text: [text], target_lang: 'EN' }),
   }).then(res => res.json())
