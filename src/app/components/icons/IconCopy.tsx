402e294f287251c4da661e5327f971fed3b9744d
min9nim <min9nim@gmail.com>
2023-12-06 22:34:42 +0900

    new icons

---
+++ src/app/components/icons/IconCopy.tsx
@@ -0,0 +1,16 @@
+export default function IconCopy({ size }) {
+  return (
+    <svg
+      width={`${size}pt`}
+      height={`${size}pt`}
+      version="1.1"
+      viewBox={`0 0 1200 1200`}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g fill="currentColor">
+        <path d="m1050 150h-670.8v670.8h670.8zm-72 598.8h-526.8v-526.8h526.8z" />
+        <path d="m319.2 451.2v-72h-169.2v670.8h670.8v-168h-72v96h-526.8v-526.8z" />
+      </g>
+    </svg>
+  )
+}
