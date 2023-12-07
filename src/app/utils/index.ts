94b5b526c8f71e08640433cff3b17f9230b5cc48
keating <min9nim@gmail.com>
2023-12-07 17:55:38 +0900

    new deeplReq

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -6,3 +6,12 @@ export const copyToClipboard = val => {
   document.execCommand('copy')
   document.body.removeChild(t)
 }
+
+export const deeplReq = (path, option) =>
+  fetch(`https://api-free.deepl.com${path}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `DeepL-Auth-Key ${process.env.API_KEY}`,
+    },
+    ...option,
+  }).then(res => res.json())
